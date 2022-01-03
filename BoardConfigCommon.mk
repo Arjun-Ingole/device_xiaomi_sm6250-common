639c90b92869844093c6b4e441b6f1310ce2b34f
Arjun-Ingole <arjuningole38@gmail.com>
2022-01-03 15:59:23 +0530

    sm6250-common: sepolicy: Clean private sepolicy
    Signed-off-by: Arjun-Ingole <arjuningole38@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -189,9 +189,6 @@ VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
-
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/public
 BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
 # Treble
